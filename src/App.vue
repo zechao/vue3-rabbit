656de0b8be1e6df9c61c6f5fc10cba8d1ee5d798
zechao <zechao158@gmail.com>
2023-10-26 01:00:19 +0200

    add element ui

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {  RouterView } from 'vue-router'
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <el-button type="primary"> Primary</el-button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
