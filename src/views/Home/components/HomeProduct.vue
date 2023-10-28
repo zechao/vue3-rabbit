a33853b54f620b2ec981f576638193cf9d0a58e7
zechao <zechao158@gmail.com>
2023-10-29 00:36:31 +0200

    add home category

---
+++ src/views/Home/components/HomeProduct.vue
@@ -0,0 +1,4 @@
+<template>
+    HomeProduct
+</template>
+        
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,20 @@
 <template>
-    <div style="height: 1300px;">
-        Home component
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
     </div>
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
 </template>
 
 <script setup>
-
+import HomeBanner from './components/HomeBanner.vue'
+import HomeCategory from './components/HomeCategory.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomeHot from './components/HomeHot.vue'
 </script>
+
 
 <style lang="scss" scoped></style>
