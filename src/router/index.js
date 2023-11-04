641e3d37924ca736bd09754d6fd1062e29ec5bbf
zechao <zechao158@gmail.com>
2023-11-04 20:17:38 +0100

    detail router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Layout from "@/views/Layout/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
+import Detail from "@/views/Detail/index.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,6 +31,10 @@ const router = createRouter({
         {
             path: "/login",
             component: Login,
+        },
+        {
+            path: "/detail/:id",
+            component: Detail,
         },
     ],
     // router behaviour
