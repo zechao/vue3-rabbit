563864d27dbd11b4c6baf403f88636d2ee15ff7e
zechao <zechao158@gmail.com>
2023-11-05 19:53:37 +0100

    global components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import {lazyPlugin} from '@/directives'
+import {componentPlugin} from '@/components'
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,8 +12,10 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(lazyPlugin)
-
 
 app.mount("#app");
 
+// directive
+app.use(lazyPlugin)
+// components
+app.use(componentPlugin)
